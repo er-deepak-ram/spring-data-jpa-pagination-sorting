1dd514c0f57c20b16e5f2c140525e601687e09bc
Deepak Ram <deepak.kumar2305@gmail.com>
2022-06-26 17:45:25 +0530

    Initial commit

---
+++ src/main/java/com/google/springdatajpapaginationsorting/SpringDataJpaPaginationSortingApplication.java
@@ -0,0 +1,13 @@
+package com.google.springdatajpapaginationsorting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaPaginationSortingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaPaginationSortingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/google/springdatajpapaginationsorting/SpringDataJpaPaginationSortingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.google.springdatajpapaginationsorting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaPaginationSortingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
